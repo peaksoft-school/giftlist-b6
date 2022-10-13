6fa7646226eac7a11b0981d2089688633ddfe99c
Chyngyz Sharshekeev <76399128+ChyngyzIshen@users.noreply.github.com>
2022-10-13 16:22:34 +0600

    refactor security v2

--- src/main/java/kg/peaksoft/giftlistb6/configs/security/WebAppSecurity.java
+++ src/main/java/kg/peaksoft/giftlistb6/configs/security/WebAppSecurity.java
@@ -1,13 +1,25 @@
 package kg.peaksoft.giftlistb6.configs.security;
 
+import kg.peaksoft.giftlistb6.repository.UserRepository;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +27,21 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebAppSecurity {
 
     @Bean
+    AuthenticationProvider authenticationProvider(UserRepository userRepo) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService((email) -> userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user with email = " + email + " not found!")));
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, TokenVerifyFilter filter) throws Exception {
 
         httpSecurity.cors().and().csrf().disable()
-                .authorizeHttpRequests(auth -> { auth
-                        .antMatchers("api/public/**").permitAll()
-                        .antMatchers("/api-docs", "/v3/api-docs")
+                .authorizeHttpRequests(auth -> {auth
+                            .antMatchers("api/public/**").permitAll()
+                            .antMatchers("/api-docs", "/v3/api-docs")
                             .permitAll()
                             .anyRequest()
                             .permitAll();
@@ -34,5 +55,33 @@ public class WebAppSecurity {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setExposedHeaders(Collections.singletonList("x-auth-token"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return new EmbeddedTomcatCustomizer();
+    }
+
+    private static class EmbeddedTomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+        @Override
+        public void customize(TomcatServletWebServerFactory factory) {
+            factory.addConnectorCustomizers(connector -> {
+                connector.setAttribute("relaxedPathChars", "<>[\\]^`{|}");
+                connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
+            });
+        }
     }
 }
