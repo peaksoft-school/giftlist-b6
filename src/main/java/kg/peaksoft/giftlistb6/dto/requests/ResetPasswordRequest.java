60a1c8264fad18025c40dc6710c397ca330ea920
Nurtilek <nurtik229494@gmail.com>
2022-10-14 17:35:15 +0600

    forgot password

---
+++ src/main/java/kg/peaksoft/giftlistb6/dto/requests/ResetPasswordRequest.java
@@ -0,0 +1,11 @@
+package kg.peaksoft.giftlistb6.dto.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResetPasswordRequest {
+    private Long id;
+    private String newPassword;
+}
