658cf281b96c8e7739add4092b3a80d7ea8c2045
Klara <azimovaklara18@gmail.com>
2022-10-20 08:14:44 +0000

    Wish List

--- src/main/java/kg/peaksoft/giftlistb6/api/WishApi.java
+++ src/main/java/kg/peaksoft/giftlistb6/api/WishApi.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/wish")
+@RequestMapping("/api/wishList")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Tag(name = "Wish Api", description = "Wish CRUD")
 public class WishApi {
@@ -23,42 +23,42 @@ public class WishApi {
     private final WishService wishService;
 
     @Operation(summary = "Save wish", description = "User can save wish")
-    @PostMapping("/save")
+    @PostMapping()
     public WishResponse saveWish(@RequestBody WishRequest wishRequest) {
         return wishService.saveWish(wishRequest);
     }
 
 
     @Operation(summary = "Update wish", description = "User can update information only own wish")
-    @PutMapping("/update/{id}")
+    @PutMapping("/{id}")
     public WishResponse updateWishById(@PathVariable Long id,
                                        @RequestBody WishRequest wishRequest) {
         return wishService.update(id, wishRequest);
     }
 
     @Operation(summary = "Delete wish", description = "User can delete wishlist, when we delete wish holiday and user will not be deleted")
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     public SimpleResponse deleteWishById(@PathVariable Long id) {
         return wishService.deleteWishById(id);
     }
 
 
     @Operation(summary = "Get wish", description = "User can get wish by id")
-    @GetMapping("/findById/{id}")
+    @GetMapping("/{id}")
     public InnerWishResponse getById(@PathVariable Long id) {
         return wishService.findById(id);
     }
 
 
     @Operation(summary = "Get all wishes", description = "User can get all wishes")
-    @GetMapping("/getAll")
+    @GetMapping()
     public List<WishResponse> getAllWishes() {
         return wishService.findAll();
     }
 
 
     @Operation(summary = "Principal", description = "Only user gives")
-    @GetMapping("/getPrincipal")
+    @GetMapping()
     public User getAuthPrincipal() {
         return wishService.getAuthPrincipal();
     }
