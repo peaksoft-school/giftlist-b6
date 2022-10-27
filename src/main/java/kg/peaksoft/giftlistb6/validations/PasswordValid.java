607b178058bfedb381d84ffa59bf4e1ca56257ab
Admin <sunatbekd@gmail.ru>
2022-10-27 14:03:52 +0300

    added validation(final)

--- src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValid.java
+++ src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValid.java
@@ -5,7 +5,7 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 
-@Target({ElementType.FIELD,ElementType.METHOD,ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD,ElementType.ANNOTATION_TYPE})
 @Documented
 @Constraint(validatedBy = PasswordValidator.class)
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValidator.java
+++ src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValidator.java
@@ -6,8 +6,8 @@ import javax.validation.ConstraintValidatorContext;
 public class PasswordValidator implements ConstraintValidator<PasswordValid, String> {
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
-        if (password.length()<6){
-            return password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$");
+        if (password.length()>6){
+            return password.matches("^[A-Za-z]{6,14}$");
         }else{
             return false;
         }
