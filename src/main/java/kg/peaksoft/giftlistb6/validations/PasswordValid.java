685915c5e2465c3ca814897462f995f6b8d68e55
Admin <sunatbekd@gmail.ru>
2022-10-28 12:23:00 +0300

    changed length password

--- src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValid.java
+++ src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValid.java
@@ -4,7 +4,6 @@ import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
-
 @Target({ElementType.FIELD,ElementType.ANNOTATION_TYPE})
 @Documented
 @Constraint(validatedBy = PasswordValidator.class)
--- src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValidator.java
+++ src/main/java/kg/peaksoft/giftlistb6/validations/PasswordValidator.java
@@ -6,8 +6,8 @@ import javax.validation.ConstraintValidatorContext;
 public class PasswordValidator implements ConstraintValidator<PasswordValid, String> {
     @Override
     public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
-        if (password.length()>6){
-            return password.matches("^[A-Za-z]{6,14}$");
+        if (password.length()>4){
+            return password.matches("^[A-Za-z]{4,14}$");
         }else{
             return false;
         }
