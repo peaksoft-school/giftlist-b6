6dc2d78419abf319c916c9fcb0bae42e34b27b19
Zhaulanbek <zhaulanbeka@gmail.com>
2022-11-16 17:18:47 +0600

    fix

--- src/main/java/kg/peaksoft/giftlistb6/api/AdminCharityApi.java
+++ src/main/java/kg/peaksoft/giftlistb6/api/AdminCharityApi.java
@@ -6,25 +6,27 @@ import kg.peaksoft.giftlistb6.db.services.AdminCharityService;
 import kg.peaksoft.giftlistb6.dto.responses.CharityResponses;
 import kg.peaksoft.giftlistb6.dto.responses.InnerCharityResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/charities")
+@RequestMapping("/api/admin/charities")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
-@Tag(name = "Admin charity Api", description = "")
+@Tag(name = "Admin charity Api", description = "Admin can see all charities ")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class AdminCharityApi {
 
     private final AdminCharityService charityService;
 
     @Operation(summary = "Get charity", description = "Get charity by id")
-    @GetMapping("{id}/admin")
+    @GetMapping("{id}")
     public InnerCharityResponse getCharityWithId(@PathVariable Long id) {
         return charityService.getCharityById(id);
     }
 
     @Operation(summary = "All charities", description = "Admin can see all charities")
-    @GetMapping("admin")
+    @GetMapping
     public CharityResponses getAllCharities() {
         return charityService.getAllCharityResponse();
     }
