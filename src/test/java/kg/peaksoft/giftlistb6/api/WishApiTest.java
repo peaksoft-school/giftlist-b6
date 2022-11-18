da65a6a7c608fb6bc6afc8044b5850f57880e385
Klara <azimovaklara18@gmail.com>
2022-11-18 12:02:36 +0000

    refactor WishList delete and update

---
+++ src/test/java/kg/peaksoft/giftlistb6/api/WishApiTest.java
@@ -0,0 +1,26 @@
+package kg.peaksoft.giftlistb6.api;
+
+import org.junit.jupiter.api.Test;
+
+class WishApiTest {
+
+    @Test
+    void saveWish() {
+    }
+
+    @Test
+    void updateWishById() {
+    }
+
+    @Test
+    void deleteWishById() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getAllWishes() {
+    }
+}
