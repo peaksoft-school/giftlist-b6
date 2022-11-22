c78e29f6722209297623ef2d6868e6d76fa8c7ce
Nurtilek <nurtik229494@gmail.com>
2022-11-22 16:22:27 +0600

    refactor booked

--- src/main/java/kg/peaksoft/giftlistb6/dto/responses/GiftResponse.java
+++ src/main/java/kg/peaksoft/giftlistb6/dto/responses/GiftResponse.java
@@ -1,5 +1,6 @@
 package kg.peaksoft.giftlistb6.dto.responses;
 
+import kg.peaksoft.giftlistb6.db.models.Gift;
 import kg.peaksoft.giftlistb6.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,13 +13,21 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class GiftResponse {
 
     private Long id;
-    private String fullName;
+    private String name;
     private Status status;
     private LocalDate date;
     private String image;
-    private String name;
+    private ReservedUserResponse reservedUserResponse;
+
+    public GiftResponse(Gift gift){
+        this.id=  gift.getId();
+        this.name=gift.getWish().getWishName();
+        this.status = gift.getWish().getWishStatus();
+        this.image = gift.getWish().getImage();
+        this.reservedUserResponse = new ReservedUserResponse(gift.getUser().getId(),gift.getUser().getFirstName()+" "+gift.getUser().getLastName(),gift.getUser().getImage());
+    }
+
 }
