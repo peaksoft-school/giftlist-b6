549e84dbbea19bf49ef77d986ff03ee3c3152d2a
Aizada <aiza0000123@gmail.com>
2022-11-23 16:44:07 +0600

    logging

--- src/main/java/kg/peaksoft/giftlistb6/db/services/CharityService.java
+++ src/main/java/kg/peaksoft/giftlistb6/db/services/CharityService.java
@@ -206,4 +206,44 @@ public class CharityService {
         }
         return responseList;
     }
+
+    @Transactional
+    public InnerCharityResponse getCharityByIdWithAdmin(Long id) {
+        Charity charity = charityRepository.findById(id).orElseThrow(
+                ()->new NotFoundException("Не найден!")
+        );
+
+        InnerCharityResponse response = new InnerCharityResponse(charity.getId(),charity.getImage(),charity.getName(),
+                charity.getDescription(),charity.getCategory().getName(),charity.getSubCategory().getName(),
+                charity.getCondition(),charity.getCreatedAt(),charity.getCharityStatus());
+
+        ReservoirResponse reservoirResponse = new ReservoirResponse(charity);
+
+        UserCharityResponse userCharityResponse = new UserCharityResponse(charity.getUser().getId(),charity.getUser().getFirstName(),
+                charity.getUser().getLastName(), charity.getUser().getImage());
+
+        response.setUserCharityResponse(userCharityResponse);
+        if (charity.getReservoir()==null){
+            response.setReservoirResponse(new ReservoirResponse());
+        }
+        response.setReservoirResponse(reservoirResponse);
+        return response;
+    }
+
+    @Transactional
+    public CharityResponses getAllCharityResponseByAdmin() {
+        CharityResponses charityResponse = new CharityResponses();
+        List<OtherCharityResponse> otherCharityResponses = charityRepository.getAllCharities();
+        for (Charity c : charityRepository.findAll()) {
+            for (OtherCharityResponse o : otherCharityResponses) {
+                if (o.getReservoir() == null) {
+                    o.setReservoir(new ReservoirResponse());
+                }
+                ReservoirResponse reservoirResponse1 = new ReservoirResponse(o.getReservoir().getId(), o.getReservoir().getImage());
+                o.setReservoir(reservoirResponse1);
+            }
+            charityResponse.setOtherCharityResponses(otherCharityResponses);
+        }
+        return charityResponse;
+    }
 }
