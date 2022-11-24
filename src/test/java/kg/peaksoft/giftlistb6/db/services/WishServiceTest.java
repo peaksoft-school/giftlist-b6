35503105b48f76bc17e99c93131672dee27626f7
Klara <azimovaklara18@gmail.com>
2022-11-24 18:38:41 +0000

    test.

--- src/test/java/kg/peaksoft/giftlistb6/db/services/WishServiceTest.java
+++ src/test/java/kg/peaksoft/giftlistb6/db/services/WishServiceTest.java
@@ -1,9 +1,11 @@
 package kg.peaksoft.giftlistb6.db.services;
 
-import kg.peaksoft.giftlistb6.api.WishApi;
+import kg.peaksoft.giftlistb6.db.models.Holiday;
+import kg.peaksoft.giftlistb6.db.models.User;
 import kg.peaksoft.giftlistb6.db.models.Wish;
 import kg.peaksoft.giftlistb6.db.repositories.WishRepository;
 import kg.peaksoft.giftlistb6.dto.responses.WishResponse;
+import kg.peaksoft.giftlistb6.enums.Status;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,30 +15,28 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
 import org.webjars.NotFoundException;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
-;
 
 //@DataJpaTest
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 class WishServiceTest {
 
-    @InjectMocks
-    private WishService wishService;
     @Mock
     private WishRepository wishRepository;
 
-//    public WishServiceTest() {
-//        this.wishRepository = mock(WishRepository.class);
-//        this.wishService = new WishService(wishRepository);
-//    }
+    @InjectMocks
+    private WishService wishService;
 
     @LocalServerPort
     private String port;
@@ -49,26 +49,59 @@ class WishServiceTest {
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
         log.info("after each!");
     }
 
-
     @Test
     void saveWish() {
-
-//        when(wishService.saveWish(wishResponse)).then(invocationOnMock ->
-//                doThrow());
-//        wishService.saveWish(wishResponse);
-//        when(wishService.saveWish(null))
-//                .thenReturn(new WishResponse())
-//                .thenReturn(null);
-//        WishResponse save = wishService.saveWish(null);
-//        System.out.println(save);
+        Wish wish = Wish.builder()
+                .id(1L)
+                .wishName("")
+                .holiday(new Holiday())
+                .dateOfHoliday(LocalDate.of(2022, 11, 18))
+                .wishStatus(Status.RESERVED)
+                .image("")
+                .description("")
+                .linkToGift("http://link")
+                .complaints(new ArrayList<>())
+                .isBlock(true)
+                .reservoir(new User())
+                .build();
+        WishResponse wishes = wishService.mapToResponse(wish);
+        assertAll(() -> assertEquals(wishes.getWishName(), wish.getWishName()));
     }
 
     @Test
+    @Sql(scripts = "/scripts/wishes.sql")
+    void findById() {
+        Wish wish = wishRepository.findById(1L)
+                .orElseThrow(() -> new NotFoundException("Не найден"));
+        assertEquals(wish.getId(), 1L);
+//        doReturn(new WishResponse()).when(wishService).findById(anyLong());
+//        wishService.findById(anyLong());
+    }
+
+    @Test
+    @Sql(scripts = "/scripts/wishes.sql")
+    void findAll() {
+        List<WishResponse> all = wishService.findAll();
+        assertEquals(Collections.emptyList(), all);
+        assertNotNull(wishService);
+    }
+
+    @Test
+    @Sql(scripts = "/scripts/wishes.sql")
     void update() {
+        Wish wish = wishRepository.findById(1L)
+                .orElseThrow(() -> new NotFoundException("Не найден"));
+        wish.setWishName("Phone");
+        Wish savedWish = wishRepository.save(wish);
+        log.info("wish name = {}", savedWish.getWishName());
+        assertEquals(
+                savedWish.getWishName(),
+                "Phone"
+        );
     }
 
     @Test
@@ -76,20 +109,7 @@ class WishServiceTest {
         when(wishRepository.existsById(1L)).thenReturn(true);
         assertThrows(NotFoundException.class, () -> {
             wishService.deleteWishById(1L);
-        }, "Not exception");
+        }, "Не найден");
 //        assertNotNull(wishService);
-    }
-
-    @Test
-    void findById() {
-        doReturn(new WishResponse()).when(wishService).findById(anyLong());
-        wishService.findById(anyLong());
-    }
-
-    @Test
-    void findAll() {
-        List<WishResponse> all = wishService.findAll();
-        assertEquals(Collections.emptyList(), all);
-        assertNotNull(wishService);
     }
 }
---
+++ src/test/resources/scripts/wishes.sql
@@ -0,0 +1,6 @@
+INSERT INTO wishes(id, date_of_holiday, description, image, link_to_gift, wish_name, wish_status, is_block, holiday_id,
+                   reservoir_id, user_id)
+VALUES (1, '2020-12-03', 'роман', null, null, 'книга', 'RESERVED', false, 1, 4, 3),
+       (2, '2022-07-05', 'телефон', null, null, 'iphone 14pro', 'RESERVED', false, 2, 3, 4),
+       (3, '2022-01-09', 'шоколад', null, null, 'sneakers', 'RESERVED', false, 3, 3, 4),
+       (4, '2020-02-09', 'ноутбук', null, null, 'macbook air pro', 'WAIT', false, 4, null, 4);
