e6e314a7fffce594f4e82f73cb36f3773e510fcb
Jusay <98471925+maksat990rd@users.noreply.github.com>
2022-12-01 18:38:48 +0600

    save test

--- src/main/java/kg/peaksoft/giftlistb6/db/models/MailingList.java
+++ src/main/java/kg/peaksoft/giftlistb6/db/models/MailingList.java
@@ -1,6 +1,6 @@
 package kg.peaksoft.giftlistb6.db.models;
 
-import kg.peaksoft.giftlistb6.dto.requests.MailingListRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MailingList {
 
     @Id
---
+++ src/test/java/kg/peaksoft/giftlistb6/db/services/MailingListServiceTest.java
@@ -0,0 +1,33 @@
+package kg.peaksoft.giftlistb6.db.services;
+
+import kg.peaksoft.giftlistb6.db.models.MailingList;
+import kg.peaksoft.giftlistb6.db.repositories.MailingListRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource("/application-test.yml")
+class MailingListServiceTest {
+
+    @Autowired
+    private MailingListRepository repository;
+
+    @Test
+    @Order(1)
+    @Rollback(value = false)
+    void saveMailingList() {
+        MailingList mailingList = new MailingList(1L,"test","test","test",
+                LocalDateTime.now());
+        Assertions.assertThat(mailingList.getId()).isGreaterThan(0);
+    }
+}
