1b204b41f86abb7d2a6afa6aec05ce7020cc63fe
Jusay <98471925+maksat990rd@users.noreply.github.com>
2022-12-16 11:45:07 +0600

    fixed junit test mailing list2

--- src/test/java/kg/peaksoft/giftlistb6/db/services/MailingListServiceTest.java
+++ src/test/java/kg/peaksoft/giftlistb6/db/services/MailingListServiceTest.java
@@ -3,15 +3,11 @@ package kg.peaksoft.giftlistb6.db.services;
 import kg.peaksoft.giftlistb6.db.models.MailingList;
 import kg.peaksoft.giftlistb6.db.repositories.MailingListRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
 import java.util.List;
