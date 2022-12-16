bcff2b14da3f77ad9e346f1d964d7a5ad9f953ac
Aizada <aiza0000123@gmail.com>
2022-12-16 18:10:45 +0600

    JUnit test friends

--- src/test/java/kg/peaksoft/giftlistb6/db/services/FriendServiceTest.java
+++ src/test/java/kg/peaksoft/giftlistb6/db/services/FriendServiceTest.java
@@ -2,31 +2,33 @@ package kg.peaksoft.giftlistb6.db.services;
 
 import kg.peaksoft.giftlistb6.db.models.User;
 import kg.peaksoft.giftlistb6.db.repositories.FriendRepository;
-import kg.peaksoft.giftlistb6.db.repositories.UserRepository;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestPropertySource("/application-test.yml")
 class FriendServiceTest {
 
-    @Autowired
-    private UserRepository userRepository;
     @Autowired
     private FriendRepository friendRepository;
 
     @Test
     void getAllFriends() {
-        User user = userRepository.findById(5L).get();
+        User user = new User("User");
         int size = friendRepository.getAllFriends(user.getEmail()).size();
         assertThat(size).isGreaterThan(0);
     }
 
     @Test
     void getAllRequests() {
-        User user = userRepository.findById(3L).get();
+        User user = new User("User");
         int size = friendRepository.getAllRequests(user.getEmail()).size();
         assertThat(size).isGreaterThan(0);
     }
